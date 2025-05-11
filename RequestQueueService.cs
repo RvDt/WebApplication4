0a32ae85115d8078b37386bc2500b873a3bb6496
Dmitry <_email@example.com>
2025-05-11 16:46:54 +0400

    1

--- RequestQueueService.cs
+++ RequestQueueService.cs
@@ -8,7 +8,7 @@
     public class RequestQueueService
     {
         private readonly SemaphoreSlim _semaphore = new(1); // Только один запрос за раз
-        private readonly TimeSpan _delayBetweenRequests = TimeSpan.FromSeconds(3); // Задержка между запросами
+        private readonly TimeSpan _delayBetweenRequests = TimeSpan.FromSeconds(5); // Задержка между запросами
 
         public async Task<T> Enqueue<T>(Func<Task<T>> func)
         {
