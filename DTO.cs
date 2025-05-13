1cce50b0e47bafd57154e0c55b10f912771cbfbe
Dmitry <_email@example.com>
2025-05-13 20:17:03 +0400

    1

--- DTO.cs
+++ DTO.cs
@@ -3,6 +3,9 @@
     public class CookieRequest
     {
         public string Cookies { get; set; }
+        public string AndroidVersion { get; set; }
+        public string DeviceManufacturer { get; set; }
+        public string DeviceModel { get; set; }
     }
 
     public class AuthResponse
